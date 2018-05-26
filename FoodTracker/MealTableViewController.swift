7d60b45ed8b8c6832952f025d20c47eb4d270484
tham gia huy <>
2018-05-26 10:07:01 +0700

    fix thêm giữ liệu mới

--- FoodTracker/MealTableViewController.swift
+++ FoodTracker/MealTableViewController.swift
@@ -29,6 +29,7 @@ class MealTableViewController: UITableViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         tableView.reloadData()
+        
     }
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
